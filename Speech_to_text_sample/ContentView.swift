49d9276ec5a4d9bee43c7ae06091cf94c2d18f82
Rintaro-Fukui <s2122098@stu.musashino-u.ac.jp>
2021-12-03 09:37:49 +0900

    Initial Commit

---
+++ Speech_to_text_sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Speech_to_text_sample
+//
+//  Created by RintaroFukui on 2021/12/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Speech_to_text_sample/Speech_to_text_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Speech_to_text_sampleApp.swift
+//  Speech_to_text_sample
+//
+//  Created by RintaroFukui on 2021/12/03.
+//
+
+import SwiftUI
+
+@main
+struct Speech_to_text_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
